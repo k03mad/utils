837eb5570f5e4d7e27642eb8e21e9041cc8218c5
k03mad <k03.mad@gmail.com>
2021-07-31 03:27:11 +0300

    add slash

--- app/api/syncthing/get.js
+++ app/api/syncthing/get.js
@@ -8,7 +8,7 @@ const {syncthing} = require('../../../env');
  * @returns {Promise<object>}
  */
 module.exports = async path => {
-    const {body} = await got(`http://${syncthing.ip}:${syncthing.port}/rest${path}`, {
+    const {body} = await got(`http://${syncthing.ip}:${syncthing.port}/rest/${path}`, {
         headers: {
             'X-API-Key': syncthing.key,
         },
