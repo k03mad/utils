723e2317de1bf446422e7198aa0e0bff7f8d59b5
k03mad <k03.mad@gmail.com>
2021-11-23 01:57:23 +0300

    folder trim path

--- app/utils/folder/size.js
+++ app/utils/folder/size.js
@@ -4,14 +4,20 @@ const run = require('../shell/run');
 
 /**
  * @param {string} path
+ * @param {object} opts
+ * @param {string} opts.trim
  * @returns {Promise<object>}
  */
-module.exports = async path => {
+module.exports = async (path, {trim} = {}) => {
     const sizes = {};
     const du = await run(`sudo du -s ${path}`);
 
     [...du.matchAll(/(\d+)\s+([\w/-]+)/g)]
         .forEach(([, count, folder]) => {
+            if (trim) {
+                folder = folder.replace(trim, '');
+            }
+
             sizes[folder] = Number(count);
         });
 
